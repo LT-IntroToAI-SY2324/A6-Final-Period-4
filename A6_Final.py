aff48731496d0dc623944977395c72eaf7c077f0
Bianca Marchan <bimarchan@cps.edu>
2023-12-19 10:21:09 -0600

    Changed

--- A6_Final.py
+++ A6_Final.py
@@ -1,1 +1,10 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+
+data = pd.read_csv("part1-linear-regression/blood_pressure_data.csv")
+print(data)
+x = data["Year"].values
+y = data["Stock Price"].values
 
